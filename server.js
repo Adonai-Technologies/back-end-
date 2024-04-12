0e7a9333a6f2c9f1fceca1209f84a41c5ff289fa
Akua Konadu <akuakkonadu8@gmail.com>
2024-04-12 15:46:24 +0100

    created vercel.json

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const connectDB = require("./Config/database");
 const portfolioRoute = require("./Routes/portfolioRoute");
 const bodyParser = require("body-parser");
 require("dotenv").config({ path: "./Config/.env" });
-connectDB();
+connectDB(); // Connect to MongoDB
 
-// Use body-parser for parsing JSON
+// Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 // Serve static files from the "public" directory
@@ -18,15 +18,20 @@ app.use(express.urlencoded({ extended: true }));
 // Use your API routes
 app.use("/api", portfolioRoute);
 
-// Start the server
+// Serve React static files in production
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname + "/client/build/index.html"));
-	});
+  // Serve static files from the "client/build" directory
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // Route all other requests to the React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-app.listen(process.env.PORT, () => {
-	console.log("Server is running, you better catch it!");
+// Start the server
+const PORT = process.env.PORT || 2424; // Use default port 5000 if not specified in .env
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
