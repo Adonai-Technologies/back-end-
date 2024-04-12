e173bc096faac7a1aebac41a389bb62d09eef122
Akua Konadu <akuakkonadu8@gmail.com>
2024-04-12 15:04:56 +0100

    new

--- Config/database.js
+++ Config/database.js
@@ -1,19 +1,18 @@
-// Config/database.js
-
 const mongoose = require('mongoose');
-const dbURI = 'mongodb://localhost:27017/myporfoliowebsites';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(dbURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        process.exit(1); // Exit process on failure
-    }
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Other options can be added here if needed
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDB;
