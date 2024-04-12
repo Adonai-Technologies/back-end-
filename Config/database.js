9352280db130f0dea45afe35d33fcc5e75ee1e94
Akua Konadu <akuakkonadu8@gmail.com>
2024-04-12 15:00:19 +0100

    updated

--- Config/database.js
+++ Config/database.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.DB_STRING, {
-     
-    })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Other options can be added here if needed
+    });
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`)
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); // Exit process with failure
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
